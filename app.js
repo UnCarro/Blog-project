3cce4decec222b8ab30eae0aca9e1ccc878cce07
Carro De Carreras Cardenas Medina <bcv10g@hotmail.com>
2021-03-04 15:42:59 -0600

    para que se quiten las notificaciones molestas

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-//jshint esversion:6
-
+//Requirir módulos de Node.js
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-
+const _ = require("lodash");
+//
+//Dummys
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent =
   "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+//
 
 const app = express();
 
+//especificaciones de uso para los módulos de Node.js
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -24,6 +27,22 @@ app.get("/", function (req, res) {
     homeStarting: homeStartingContent,
     post: posts,
   });
+});
+
+app.get("/posts/:postName", function (req, res) {
+  let requestedTitle = req.params.postName;
+  posts.forEach(function (post) {
+    const storedTitle = post.title;
+    if (requestedTitle === storedTitle) {
+      console.log("Match found");
+    } else {
+      console.log("Not a match");
+    }
+    console.log(posts);
+  });
+
+  res.render("post", { post: posts });
+  console.log(req.params.postName);
 });
 
 app.get("/contact", function (req, res) {
@@ -47,10 +66,10 @@ app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
 
+//
+
+//
+
 //Provisiones.
 //app.get("/", function (res, req) {});
 //app.post("/", function (res, req) {});
-
-//
-
-//
