722d2fbf1587795257c198069befca7af4ca3236
Carro De Carreras Cardenas Medina <bcv10g@hotmail.com>
2021-03-04 22:35:17 -0600

    a

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 //
+
 //Dummys
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -33,7 +34,9 @@ app.get("/posts/:postName", function (req, res) {
   let requestedTitle = req.params.postName;
   posts.forEach(function (post) {
     const storedTitle = post.title;
-    if (requestedTitle === storedTitle) {
+    const lRequestedTitle = _.toLower(requestedTitle);
+    const lStoredTitle = _.toLower(storedTitle);
+    if (lRequestedTitle === lStoredTitle) {
       console.log("Match found");
     } else {
       console.log("Not a match");
@@ -42,7 +45,6 @@ app.get("/posts/:postName", function (req, res) {
   });
 
   res.render("post", { post: posts });
-  console.log(req.params.postName);
 });
 
 app.get("/contact", function (req, res) {
